79b8fe8cf94137baba58eec54753077cb8bd5df8
k4rma <fourteen2549@gmail.com>
2026-06-22 18:40:11 +0700

    Add/Update Gamble With Your Friends

---
+++ lua/3892270.lua
@@ -0,0 +1,6 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(3892270)
+addappid(3892271,0,"90695be6010d5ec2e8890f1d742357316f4d5f74a8a9d22b2ae076b184797167")
