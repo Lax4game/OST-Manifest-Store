59a820b235fbcda213c6d8e537fd29beb9ae6831
{none} <144694098+Lax4game@users.noreply.github.com>
2026-08-17 09:09:57 +0700

    Upload 2322010.lua via Admin Monitor

---
+++ lua/2322010.lua
@@ -0,0 +1,7 @@
+-- This lua file has been fetched from the bot VICTOR which is the exclusive property of Piracy Lords server.
+-- Redistribution of VICTORs files is not allowed AT ALL
+-- Join the official here: https://discord.gg/piracylords
+
+addappid(2322010)
+addappid(2322011,0,"78758c85a2a8b04770fb4fe430ac7a215a959b72e496d94f6a0ec5ac38066f49")
+addappid(2974330)
